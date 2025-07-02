9d1e5891b60cb5723236500b6b90130a0908710c
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-07-02 09:46:22 +0200

    Add new status's for stages of making a order.

--- ScreenProducerAPI/Util/Status.cs
+++ ScreenProducerAPI/Util/Status.cs
@@ -5,7 +5,10 @@ public static class Status
     public static readonly string WaitingForCollection = "waiting_collection";
     public static readonly string WaitingForPayment = "waiting_payment";
     public static readonly string Collected = "collected";
+    public static readonly string Abandoned = "abandoned";
+    public static readonly string RequiresPaymentToSupplier = "requires_payment_supplier";
+    public static readonly string RequiresDelivery = "requires_delivery";
+    public static readonly string RequiresPaymentToLogistics = "requires_payment_delivery";
     public static readonly string WaitingForDelivery = "waiting_delivery";
     public static readonly string Delivered = "delivered";
-    public static readonly string Abandoned = "abandoned";
 }
