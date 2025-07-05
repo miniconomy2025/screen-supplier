012b41b8347c26f7f298c1c203420011c0435bde
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-07-05 20:48:01 +0200

    fix overlapping subnet

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -85,7 +85,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "af-south-1b"
   map_public_ip_on_launch = true
 }
