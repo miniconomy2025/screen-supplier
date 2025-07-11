28980bfe6e5031c5b6433cf127509473822c6279
Alexander van der Leek <Alexander.VanderLeek@bbd.co.za>
2025-07-11 07:53:03 +0200

    fix retry logic

--- backend/Models/Responses/BankAccountBalanceResponse.cs
+++ backend/Models/Responses/BankAccountBalanceResponse.cs
@@ -6,4 +6,6 @@ public class BankAccountBalanceResponse
 {
     [JsonPropertyName("net_balance")]
     public int Balance { get; set; }
+    [JsonPropertyName("account_number")]
+    public string AccountNumber { get; set; }
 }
