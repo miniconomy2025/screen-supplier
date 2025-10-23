514006be84a530024e2b7944da906cd20d0852ad
Bernard Paetzold <bernard.paetzold@bbd.co.za>
2025-10-23 10:16:18 +0200

    Queue tests

--- backend/Services/SupplierService/Recycler/Models/RecyclerMaterialsResponse.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace ScreenProducerAPI.Services.SupplierService.Recycler.Models;
-
-public class RecyclerMaterialsResponse
-{
-    public List<RecyclerMaterial> Materials { get; set; } = [];
-}
