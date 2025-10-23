938928b96dec2fe59d97586f85d0e97609e5e699
VuyoSibiya-BBD <vuyo.sibiya@bbd.co.za>
2025-10-23 18:53:21 +0200

    test setup

---
+++ ScreenProducerAPI.IntegrationTests/Tests/Endpoints/TargetQuantityEndpointsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using ScreenProducerAPI.IntegrationTests.Fixtures;
+using ScreenProducerAPI.Models;
+using ScreenProducerAPI.ScreenDbContext;
+using ScreenProducerAPI.Services;
+using ScreenProducerAPI.Util;
+
+namespace ScreenProducerAPI.IntegrationTests.Tests.Endpoints;
+
+[TestFixture]
+public class TargetQuantityEndpointTests
+{
+    private CustomWebApplicationFactory _factory = null!;
+    private HttpClient _client = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _factory = new CustomWebApplicationFactory();
+        _client = _factory.CreateClient();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _client?.Dispose();
+        _factory?.Dispose();
+    }
+
+    
+}
